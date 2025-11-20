fe72a5ff66b67f017bfad28ea7c6769abfa3df70
DiegoJT22 <daigoestrlar22@gmail.com>
2025-11-20 12:12:50 -0500

    Problema 1

--- problematicas/Problematica1/src/problematica1/Problematica1.java
+++ problematicas/Problematica1/src/problematica1/Problematica1.java
@@ -1,20 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package problematica1;
 
-/**
- *
- * @author daigo
- */
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Problematica1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        int contador = 1;
+        double promedad = 0;
+        double promesta = 0;
+        int edad;
+        double estatura;
+        boolean bandera = true;
+        String salida;
+        String posicion;
+        String nombre;
+        String mensaje = "";
+        String lista = "";
+
+        while (bandera) {
+
+            System.out.println("Ingrese nombre de jugador:");
+            nombre = entrada.nextLine();
+
+            System.out.println("Ingrese posicion de jugador:");
+            posicion = entrada.nextLine();
+
+            System.out.println("Ingrese edad:");
+            edad = entrada.nextInt();
+
+            System.out.println("Ingrese estatura:");
+            estatura = entrada.nextDouble();
+
+            promedad += edad;
+            promesta += estatura;
+
+            mensaje += String.format("%s. %s - %s, edad %d, estatura %.2f\n",
+                    contador, nombre, posicion, edad, estatura);
+
+            lista += String.format("%d\n", edad);
+
+            entrada.nextLine(); // Limpia buffer
+
+            System.out.println("Escriba 'si' para terminar o ENTER para continuar:");
+            salida = entrada.nextLine();
+
+            if (salida.equalsIgnoreCase("si")) {
+                bandera = false;
+            }
+
+            contador++;
+        }
+
+        int totalJugadores = contador - 1;
+
+        promedad /= totalJugadores;
+        promesta /= totalJugadores;
+
+        System.out.printf("\n--- RESULTADOS ---\n" +
+                "Lista de jugadores:\n%s\n" +
+                "Lista de edades:\n%s\n" +
+                "Promedio edad: %.2f\n" +
+                "Promedio estatura: %.2f\n",
+                mensaje, lista, promedad, promesta);
     }
-    
 }
+
